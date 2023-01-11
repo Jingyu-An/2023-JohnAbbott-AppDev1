71d5a945b2cdc678d0d7589711b7b98c25734ff4
Jingyu <103467264+Jingyu-An@users.noreply.github.com>
2023-01-11 05:08:38 +0000

    [AppDev1] Day02 - Task1 : All Inputs

---
+++ Day02AllInputs/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Day02AllInputs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CBoxCat_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CBoxDog_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CBoxOther_Checked(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = TbxName.Text;
+            if (name == "")
+            {
+                MessageBox.Show("Name must not be empty", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            MessageBox.Show($"Hello {name}, nice to meet you!", "Greeting", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
